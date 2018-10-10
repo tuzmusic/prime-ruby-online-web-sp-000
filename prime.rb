8101d73bb0136a3be1241cccfe492ff97889452a
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 17:32:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,11 +5,9 @@ def prime?(num)
   return false if num < 2 || (num > 3 && num.even?)
   # now we're dealing only with odd numbers greater than 4
   highest_factor = (num / 3).floor
-  possible_factors = (5..highest_factor)#.select {|n| n.odd?}
-  # binding.pry
-  # puts "testing #{possible_factors.size} factors of #{num}."
+  possible_factors = (5..highest_factor)
   !possible_factors.any? { |factor|
-    return true if factor > num/factor
+true if factor > num/factor
     num % factor == 0
   }
 end
