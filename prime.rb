e6cac32ae6b3e172cf98d747c2b5bc885c114743
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 17:01:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,11 @@
 def prime(num)
-  return true if num < 4 || (num > 3 && num.even?)
-
+  return true if num < 4
+  return false if num > 3 && num.even?
+  # now we're dealing only with odd numbers greater than 4
+  highest_factor = (num / 2).floor
+  possible_factors = (1..highest_factor)
+  possible_factors.each { |factor|
+    return false if num % factor == 0
+  }
+  return true
 end
