5a250bbbbf31d050997c639ff6466f3297c44909
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 17:22:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -9,7 +9,7 @@ def prime?(num)
   # binding.pry
   # puts "testing #{possible_factors.size} factors of #{num}."
   !possible_factors.any? { |factor|
-    num.odd? && num % factor == 0
+    num % factor == 0
   }
   # return true
 end
