8f30e279c2dcc7f823d0d2057a209880faf7afa8
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 17:33:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+require 'benchmark'
+
+def prime?(num)
+  return false if num < 2 || (num > 3 && num.even?)
+  # now we're dealing only with odd numbers greater than 4
+  highest_factor = (num / 3).floor
+  possible_factors = (5..highest_factor)
+  !possible_factors.any? { |factor|
+    return true if factor > num/factor
+    num % factor == 0
+  }
+end
+
+puts Benchmark.measure{
+  5.times {
+    prime?(2999999)
+  }
+}
