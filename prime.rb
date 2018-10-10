54bbacf289859415c02d27c656538e3a11f1ee23
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 17:04:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def prime?(num)
   return true if  num < 4 && num > 0
-  return false if num < 0 || (num > 3 && num.even?)
+  return false if num < 1 || (num > 3 && num.even?)
   # now we're dealing only with odd numbers greater than 4
   highest_factor = (num / 2).floor
   possible_factors = (2..highest_factor)
